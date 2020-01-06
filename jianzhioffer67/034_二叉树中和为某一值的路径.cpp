796524acc277c588617caa95c09dbd06539ff377
zifans <903953451@qq.com>
2020-01-06 20:14:14 +0800

    提交第50题的第一小问

--- jianzhioffer67/034_二叉树中和为某一值的路径.cpp
+++ jianzhioffer67/034_二叉树中和为某一值的路径.cpp
@@ -7,15 +7,32 @@
 
 /*
 思想：
+（1）首先该题是基于递归去遍历整棵树，遍历完每一条路径，遍历的顺序是先根节点，然后是左节点，接着是右节点；
+（2）如果节点的左右子树都为空，且路径之和等于参数，就说明该路径是需要输出的
+（3）如果不满足条件，在遍历完之后需要把最后一颗节点弹出来。
 */
-
 
 class Solution {
 public:
-    vector<vector<int> > FindPath(TreeNode* root, int expectNumber) {
-        vector<vector<int> > ans;
+    vector<vector<int> > FindPath(TreeNode* root,int expectNumber) {
         if(root == nullptr)
             return ans;
-        
+        DFS(root, expectNumber);
+        return ans;
     }
+    void DFS(TreeNode *root, int expectNumber) {
+        temp.push_back(root->val);
+        if(expectNumber - root->val == 0 && root->left == nullptr && root->right == nullptr) {
+            ans.push_back(temp);
+        } else {
+            if(root->left != nullptr)
+                DFS(root->left, expectNumber - root->val);
+            if(root->right != nullptr)
+                DFS(root->right, expectNumber - root->val);
+        }
+        temp.pop_back();
+    }
+private:
+    vector<vector<int>> ans;
+    vector<int> temp;
 };
