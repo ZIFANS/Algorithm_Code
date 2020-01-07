bf4a4fd96ed749fcf049185a1f549c786609bb22
zifans <903953451@qq.com>
2020-01-07 20:58:46 +0800

    第38题

--- jianzhioffer67/058_翻转单词顺序.cpp
+++ jianzhioffer67/058_翻转单词顺序.cpp
@@ -38,3 +38,21 @@ public:
             swap(str[s++], str[e--]);
     }
 };
+
+
+// 一个个处理
+class Solution {
+public:
+    string ReverseSentence(string str) {
+        string res = "", tmp = "";
+        for(unsigned int i = 0; i < str.size(); ++i){
+            if(str[i] == ' ') {
+                res = " " + tmp + res;
+                tmp = "";
+            }
+            else tmp += str[i];
+        }
+        if(tmp.size()) res = tmp + res;
+        return res;
+    }
+}; 
