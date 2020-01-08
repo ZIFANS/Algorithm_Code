2256e5a6b2248aefb12ff44fddf8b6ef50d55778
zifans <903953451@qq.com>
2020-01-08 11:46:19 +0800

    第29题

--- jianzhioffer67/029_顺时针打印矩阵.cpp
+++ jianzhioffer67/029_顺时针打印矩阵.cpp
@@ -10,13 +10,37 @@
 */
 
 /*
-思想：
+思想：他人的思路
+用左上和右下的坐标定位出一次要旋转打印的数据，一次旋转打印结束后，往对角分别前进和后退一个单位。
+提交代码时，主要的问题出在没有控制好后两个for循环，需要加入条件判断，防止出现单行或者单列的情况。
 */
 
 class Solution {
 public:
     vector<int> printMatrix(vector<vector<int> > matrix) {
         vector<int> ans;
-        
+        int row = matrix.size();
+        int col = matrix[0].size();
+        if(row == 0 || col == 0)
+            return ans;
+        int left = 0, top = 0, right = col - 1, bottom = row - 1;
+        while(left <= right && top <= bottom) {
+            // left to right
+            for(int i = left; i <= right; ++i) 
+                ans.push_back(matrix[top][i]);
+            // top to bottom
+            for(int i = top + 1; i <= bottom; ++i) 
+                ans.push_back(matrix[i][right]);
+            // right to left
+            if(top != bottom)
+                for(int i = right - 1; i >=left; --i)
+                    ans.push_back(matrix[bottom][i]);
+            // bottom to top
+            if(left != right) 
+                for(int i = bottom - 1; i > top; --i)
+                    ans.push_back(matrix[i][left]);
+            left++, top++, right--, bottom--;
+        }
+        return ans;
     }
 };
