dbf888c4cf1693d673de5109f0eed8ea343aaa64
zifans <903953451@qq.com>
2020-01-12 11:59:47 +0800

    第283题代码

---
+++ leetcode/JavaScript实现/0283.js
@@ -0,0 +1,15 @@
+/* 先将nums数组中非0的元素，放到index = 0开始
+然后将 0元素添加到index后面
+ */
+var moveZeroes = function(nums) {
+    let len = nums.length, index = 0;
+    for(let i = 0; i < len; ++i) {
+        if(nums[i] !== 0) {
+            nums[index] = nums[i];
+            ++index;
+        }
+    }
+    for(; index < len; ++index)
+        nums[index] = 0;
+};
+
