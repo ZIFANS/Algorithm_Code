90918294005356419c475c54895d5a2ee3bbb394
zifans <903953451@qq.com>
2020-01-17 11:40:21 +0800

    287增加一种写法

--- leetcode/JavaScript实现/0191.js
+++ leetcode/JavaScript实现/0191.js
@@ -10,9 +10,10 @@ var hammingWeight = function(n) {
 };
 
 // 第二种解法，也是基于位运算
+// 感觉这个比第一种好理解
 var hammingWeight = function(n) {
     let ans = 0;
-    let flag = 1;
+    let flag = 1;   // 首先把n与1 做 与 运算符。 ‘与’运算是两个都为1才是1，其他都是0.
     while(flag) {
         if(n & flag)
             ++ans;
--- leetcode/JavaScript实现/0287.js
+++ leetcode/JavaScript实现/0287.js
@@ -1,13 +1,28 @@
-// 第一想法排序，AC了，但是题目描述没看清
+// 第一想法排序，AC了，但是题目描述没看清，不满足题目描述
+// 自己的代码
+// 第二想法,用indexOf和 lastIndexOf，找到一个不相等的。
+// indexOf从头开始找，lastIndexOf从尾开始找
 var findDuplicate = function(nums) {
-    nums.sort((a, b) => {
-        return a - b;
-    });
     for(let i = 0; i < nums.length; ++i) {
-        if(nums[i - 1] === nums[i])
+        if(nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i]))
             return nums[i];
     }
     return -1;
 };
 
-// 第二想法
+// 第三想法借鉴别人的
+// 参考链接：https://leetcode-cn.com/problems/find-the-duplicate-number/solution/287xun-zhao-zhong-fu-shu-by-kirsche/
+var findDuplicate = function(nums) {
+    let slow = 0, fast = 0;
+    slow = nums[slow], fast = nums[nums[fast]];
+    while(slow !== fast) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }
+    let pre1 = 0, pre2 = slow;
+    while(pre1 !== pre2) {
+        pre1 = nums[pre1];
+        pre2 = nums[pre2];
+    }
+    return pre1;
+};
