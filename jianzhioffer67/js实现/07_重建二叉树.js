0bf4da65c2748125bafa9b2e1820aee3d74638f6
zifans <903953451@qq.com>
2020-02-14 15:00:43 +0800

    先序中序确定二叉树

--- jianzhioffer67/js实现/07_重建二叉树.js
+++ jianzhioffer67/js实现/07_重建二叉树.js
@@ -1,2 +1,23 @@
 // 自己的代码
 // 只要有中序和其他遍历的序列，就可以确定一颗二叉树
+var buildTree = function(preorder, inorder) {
+    if (inorder.length === 0 && preorder.length === 0)
+        return null;
+    let root = {};
+    root.val = preorder[0];
+    let rootIdxInOrder = inorder.indexOf(root.val);
+
+    //左子树的中序遍历
+    let leftTreeInOrder = inorder.slice(0, rootIdxInOrder);
+    //左子树的先序遍历 
+    let leftTreePreOrder = preorder.slice(1, leftTreeInOrder.length + 1);
+    root.left = buildTree(leftTreePreOrder, leftTreeInOrder);
+
+    //右子树的中序遍历
+    let rightTreeInOrder = inorder.slice(rootIdxInOrder + 1);
+    //右子树的先序遍历
+    let rightTreePreOrder = preorder.slice(rootIdxInOrder + 1);
+    root.right  = buildTree(rightTreePreOrder, rightTreeInOrder);
+
+    return root;
+};
