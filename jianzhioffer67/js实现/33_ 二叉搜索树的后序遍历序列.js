30bed90b49f159913c9aef7bccdd0dddfb99b215
zifans <903953451@qq.com>
2020-02-18 15:37:56 +0800

    二分法

--- jianzhioffer67/js实现/33_ 二叉搜索树的后序遍历序列.js
+++ jianzhioffer67/js实现/33_ 二叉搜索树的后序遍历序列.js
@@ -1,3 +1,29 @@
+// 第一种递归
+var verifyPostorder = function (postorder) {
+    let len = postorder.length;
+    if (len === 0)
+        return true;
+    return help(postorder, 0, len - 1);
+};
+function help(sequence, start, end) {
+    if (start >= end)
+        return true;
+    let root = sequence[end];
+    let flag = start;
+    while (flag <= end && sequence[flag] < root) {
+        ++flag;
+    }
+    for (let i = flag; i < end; ++i) {
+        if (sequence[i] < sequence[end]) {
+            return false;
+        }
+    }
+    let ansLeft = help(sequence, start, flag - 1);
+    let ansRight = help(sequence, flag, end - 1);
+    return ansLeft && ansRight;
+}
+
+
 // 还是递归
 var verifyPostorder = function(postorder) {
     let len = postorder.length;
