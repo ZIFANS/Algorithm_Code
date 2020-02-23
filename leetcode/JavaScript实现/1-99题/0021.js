a8e935a4868f34a54b98df700348b7172c1a463b
zifans <903953451@qq.com>
2020-02-23 21:27:17 +0800

    增加递归和迭代两个解法

--- leetcode/JavaScript实现/1-99题/0021.js
+++ leetcode/JavaScript实现/1-99题/0021.js
@@ -1,35 +1,22 @@
-// 这是我第一想法，但是代码时借鉴别人的，我这个阶段对js使用链表的还不是很熟悉
+// 这是我第一想法，迭代
 var mergeTwoLists = function(l1, l2) {
-    let result = tmp = new ListNode();
-  
-    while (l1 || l2) {
-      let val;
-      if (l1 === null) {
-        tmp.next = l2;
-        break;
-      }
-      if (l2 === null) {
-        tmp.next = l1;
-        break;
-      }
-  
-      if (l1.val > l2.val) {
-        val = l2.val;
-        l2 = l2.next;
+  let newHead = new ListNode(-1);
+  let prev = newHead;
+  while (l1 !== null && l2 !== null) {
+      if (l1.val <= l2.val) {
+          prev.next = l1;
+          l1 = l1.next;
       } else {
-        val = l1.val;
-        l1 = l1.next;
+          prev.next = l2;
+          l2 = l2.next;
       }
-  
-      let node = new ListNode(val);
-      tmp.next = node;
-      tmp = node;
-    }
-  
-    return result.next;
+      prev = prev.next;
+  }
+  prev.next = l1 === null ? l2 : l1;
+  return newHead.next;
 };
 
-// 自己的代码
+// 递归
 var mergeTwoLists = function(l1, l2) {
     if(l1 === null)
         return l2;
