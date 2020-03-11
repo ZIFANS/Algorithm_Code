36f42cf147f03932163749b71b21824ab5779041
zifans <903953451@qq.com>
2020-03-11 23:38:07 +0800

    动态规划和中心拓展法

--- leetcode/JavaScript实现/1-99题/1-19题/0005_ 最长回文子串.js
+++ leetcode/JavaScript实现/1-99题/1-19题/0005_ 最长回文子串.js
@@ -26,3 +26,23 @@ var longestPalindrome = function(s) {
     return s.slice(start, end + 1);
     //return s.substring(start, end + 1); // 都行
 };
+
+// 动态规划
+var longestPalindrome = function (s) {
+    let n = s.length;
+    let res = '';
+    //let dp = Array.from(new Array(n),() => new Array(n).fill(0));
+    let dp = new Array();
+    for (let i = 0; i < n; ++i) {
+        dp[i] = new Array(n).fill(0);
+    }
+    for (let i = n - 1; i >= 0; i--) {
+        for (let j = i; j < n; j++) {
+            dp[i][j] = s[i] === s[j] && (j - i < 2 || dp[i + 1][j - 1]);
+            if (dp[i][j] && j - i + 1 > res.length) {
+                res = s.slice(i, j + 1);
+            }
+        }
+    }
+    return res;
+};
