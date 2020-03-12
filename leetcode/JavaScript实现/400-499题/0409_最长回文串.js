a36545ea750ddc9ea7e71ddcda89305281c6fed5
zifans <903953451@qq.com>
2020-03-12 13:52:32 +0800

    最长回文串hash

--- leetcode/JavaScript实现/400-499题/0409_最长回文串.js
+++ leetcode/JavaScript实现/400-499题/0409_最长回文串.js
@@ -25,3 +25,30 @@ var longestPalindrome = function(s) {
         ans += 1;
     return ans;
 };
+
+// 与上面一样，只是增加了不同遍历方法
+var longestPalindrome = function(s) {
+    let map = new Map();
+    let flag = false;
+    let ans = 0;
+
+    for (let i = 0; i < s.length; ++i) {
+        if (map.has(s[i])) {
+            map.set(s[i], map.get(s[i]) + 1);
+        } else {
+            map.set(s[i], 1);
+        }
+    }
+
+    for (let [key, val] of map) {
+        if (val % 2 === 0) 
+            ans += val;
+        else {
+            flag = true;
+            ans += val - 1;
+        }
+    }
+    if (flag)
+        ans += 1;
+    return ans;
+};
