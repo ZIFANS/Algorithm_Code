90a77d0da59cfcfcbe49e58b9ae68138fe35c7b0
zifans <903953451@qq.com>
2020-03-14 15:56:53 +0800

    递归和用数组

--- jianzhioffer67/js实现/06_从尾到头打印链表.js
+++ jianzhioffer67/js实现/06_从尾到头打印链表.js
@@ -1,15 +1,24 @@
 // 自己的代码
-// 第一种想法：从头到尾遍历，然后翻转数组。
+// 1. 从头到尾遍历，然后翻转数组。，
 var reversePrint = function(head) {
     let ans = [];
-    let p = head;
-    while (p) {
-        ans.push(p.val);
-        p = p.next;
+
+    while (head) {
+        ans.push(head.val);
+        head = head.next;
     }
+
     return ans.reverse();
 };
 
-
 // 第二种：递归
+var reversePrint = function(head) {
+    if (!head)
+        return [];
+    
+    let ans = reversePrint(head.next);
+    ans.push(head.val);
+
+    return ans;
+};
 
