ed7d31bb43f3bd08d605fd530c938b32b303b96b
zifans <903953451@qq.com>
2020-03-14 22:36:09 +0800

    动态规划和不用数组

--- jianzhioffer67/js实现/10_斐波那契数列.js
+++ jianzhioffer67/js实现/10_斐波那契数列.js
@@ -1,9 +1,28 @@
-// 与leetcode509 相同题
+// 与leetcode509 相同题 计算结果要取模。这样的题第一想法当然是递归，但是会超时
+// 1. 动态规划，数组
 var fib = function(n) {
     let ans = [0, 1];
-    for (let i = 2;i <= n; ++i) {
-        ans.push(ans[i - 1] % 1000000007 + ans[i - 2] % 1000000007);
+
+    for (let i = 2; i <= n; ++i) {
+        ans[i] = (ans[i - 1] % 1000000007 + ans[i - 2] % 1000000007);
     }
+
     return ans[n] % 1000000007;
 };
 
+// 2.不用数组
+var fib = function (n) {
+    let first = 0, second = 1;
+    let temp;
+    
+    if (n === 0 || n === 1)
+        return n;
+
+    for (let i = 2; i <= n; ++i) {
+        temp = (first + second) % 1000000007;
+        first = second;
+        second = temp;
+    }
+
+    return temp;
+};
