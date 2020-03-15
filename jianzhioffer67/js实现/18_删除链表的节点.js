deab7b6aa57cdfb70b0e062ac7368048a125245f
zifans <903953451@qq.com>
2020-03-15 22:12:40 +0800

    直接法和递归

--- jianzhioffer67/js实现/18_删除链表的节点.js
+++ jianzhioffer67/js实现/18_删除链表的节点.js
@@ -1,17 +1,36 @@
-// 开始题目看错了
+// 1. 用个哑结点
 var deleteNode = function(head, val) {
-    if (head === null)
-        return head;
-    let dummp = new ListNode(null);
-    dummp.next = head;
-    let curNode = dummp;
-    while (curNode.next) {
-        if (curNode.next.val === val) {
-            curNode.next = curNode.next.next;
-            return dummp.next;
+    if (!head)
+        return null;
+    
+    let dummy = new ListNode(null);
+    dummy.next = head;
+    let pre = dummy, cur = head;
+
+    while (cur) {
+        if (cur.val === val) {
+            pre.next = cur.next;
+            return dummy.next;
         }
-        curNode = curNode.next;
+        pre = pre.next;
+        cur = cur.next;
     }
 };
 
+// 2.递归
+var deleteNode = function(head, val) {
+    if (head.val === val)
+        return head.next;
 
+    /**
+     * 假设【1，2，3】，目标值是2
+     * 当前head是1.
+     * 本来head.next是2,但是调用deletenode函数的时候刚刚好2==2,把2（head）的下一个值3的指针返回回去
+     * 所以head.next = 3
+     * 1->3
+     * 
+    */
+    head.next = deleteNode(head.next, val);
+
+    return head;
+};
