608585865d29039ddf80de9d3c7e56086af2ea17
zifans <903953451@qq.com>
2020-03-15 11:34:07 +0800

    二分法和减少循环次数

--- jianzhioffer67/js实现/10_青蛙跳台阶的问题.js
+++ jianzhioffer67/js实现/10_青蛙跳台阶的问题.js
@@ -1,8 +1,11 @@
 // 也就是斐波那契数列的小改变
+// 1.DP
 var numWays = function(n) {
     let ans = [1, 1, 2];
+
     for (let i = 3; i <= n; ++i) {
-        ans[i] = ans[i - 1] % 1000000007 + ans[i - 2] % 1000000007;
+        ans[i] = (ans[i - 1] + ans[i - 2]) % 1000000007;
     }
-    return ans[n] % 1000000007;
+
+    return ans[n];
 };
--- jianzhioffer67/js实现/11_旋转数组的最小数字.js
+++ jianzhioffer67/js实现/11_旋转数组的最小数字.js
@@ -1,17 +1,17 @@
-// 自己的代码
-// 抖个机灵的代码
+// 自己的代码，与LeetCode 154相同
+// 1.抖个机灵的代码
 var minArray = function(numbers) {
     return Math.min(...numbers);
 };
 
-// 正常的二分法 
+// 2.正常的二分法 
 // 这样的代码考虑到了数组中有重复数的时候
 var minArray = function(numbers) {
     let left = 0, right = numbers.length - 1;
+
     while (left < right) {
-        //算中间值
         let mid = (left + right) >>> 1;
-        //如果中值大于right值，说明left那边元素比较大，缩小范围
+        //如果中和right相同，right - 1，减少right范围
         if (numbers[mid] > numbers[right]) {
             left = mid + 1;
         //如果中和right相同，right - 1，减少right范围
@@ -22,6 +22,22 @@ var minArray = function(numbers) {
             right = mid;
         }
     }
+
     //此时只剩一个了 left和right都可以
     return numbers[left];
 };
+
+
+// 3.通过减少循环次数并且缩减循环内进行运算的方法实现高效查找
+var minArray = function(numbers) {
+    let i;
+
+    for (i = 0; numbers[i] <= numbers[i + 1] && i < numbers.length; ++i);
+
+    if (numbers[i + 1] !== undefined) {
+        return numbers[i + 1];
+    }
+
+    return numbers[0];
+};
+
