8ca31eec250d0bd07f0afdccee501c44d48c626c
zifans <903953451@qq.com>
2020-03-16 22:18:38 +0800

    双指针和双指针的优化

--- jianzhioffer67/js实现/22_链表中倒数第k个节点.js
+++ jianzhioffer67/js实现/22_链表中倒数第k个节点.js
@@ -1,30 +1,35 @@
-// 自己代码，第一想法就是快慢指针，这里遍历了两次
+// 1.第一想法就是快慢指针，这里遍历了2次
 var getKthFromEnd = function(head, k) {
-    let fast = head;
-    let slow = head;
+    if (!head || k < 0)
+        return null;
 
-    while (k--) {
+    let slow = head, fast = head;
+
+    for (let i = 0; i < k; ++i) {
         fast = fast.next;
     }
+
     while (fast) {
-        fast = fast.next;
         slow = slow.next;
+        fast = fast.next;
     }
+
     return slow;
 };
 
-
-// 遍历一次
+// 2.也是双指针，slow, fast两个指针，让fast先走k步，然后slow, fast一起走，直到fast为null
+// 但是只需要一次遍历
 var getKthFromEnd = function(head, k) {
-    let i = 0;
-    let fast = head;
-    let slow = head;
+    let slow = head, fast = head;
+    let count = 0;
+
     while (fast) {
-        if (i >= k) {
+        if (count >= k) {
             slow = slow.next;
         }
+        ++count;
         fast = fast.next;
-        ++i;
     }
-    return i < k ? null : slow;
+
+    return count < k ? null : slow;
 };
