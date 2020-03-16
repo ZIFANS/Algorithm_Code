8cf732e66ac96af36cdf4ca06f8d5afddc7cf592
zifans <903953451@qq.com>
2020-03-16 22:28:54 +0800

    递归和迭代

--- jianzhioffer67/js实现/25_合并两个排序的链表.js
+++ jianzhioffer67/js实现/25_合并两个排序的链表.js
@@ -1,27 +1,13 @@
-// 自己的代码
-// 第一想法就是递归
+// 1.迭代. 最常规的思想了
 var mergeTwoLists = function(l1, l2) {
     if (!l1)
         return l2;
     if (!l2)
         return l1;
-    if (l1.val < l2.val) {
-        l1.next = mergeTwoLists(l1.next, l2);
-        return l1;
-    } else {
-        l2.next = mergeTwoLists(l1, l2.next);
-        return l2;
-    }
-};
-
-// 第二想法，迭代.
-var mergeTwoLists = function(l1, l2) {
-    if (!l1)
-        return l2;
-    if (!l2)
-        return l1;
+    
     let ans = new ListNode(null);
     let p = ans;
+
     while (l1 && l2) {
         if (l1.val < l2.val) {
             p.next = l1;
@@ -30,8 +16,26 @@ var mergeTwoLists = function(l1, l2) {
             p.next = l2;
             l2 = l2.next;
         }
-        p = p.next
+        p = p.next;
     }
+
     p.next = l1 ? l1 : l2;
+
     return ans.next;
+};
+
+// 2.递归
+var mergeTwoLists = function(l1, l2) {
+    if (!l1)
+        return l2;
+    if (!l2)
+        return l1;
+    
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
 };
