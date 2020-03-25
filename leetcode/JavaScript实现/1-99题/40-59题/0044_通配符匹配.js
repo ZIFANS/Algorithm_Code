05580d226190b40fd20a317a36f28bc512de9e41
zifans <903953451@qq.com>
2020-03-25 14:32:32 +0800

    动态规划

---
+++ leetcode/JavaScript实现/1-99题/40-59题/0044_通配符匹配.js
@@ -0,0 +1,25 @@
+// 1、动态规划
+var isMatch = function(s, p) {
+    let n = s.length;
+    let m = p.length;
+    let dp = Array.from(new Array(n + 1), () => new Array(m + 1).fill(false));
+    dp[0][0] = true;
+
+    for (let i = 1; i <= m; ++i) {
+        if (p[i - 1] === '*') {
+            dp[0][i] = dp[0][i - 1];
+        }
+    }
+
+    for (let i = 1; i <= n; ++i) {
+        for (let j = 1; j <= m; ++j) {
+            if (s[i - 1] === p[j - 1] || p[j - 1] === '?') {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else if (p[j - 1] === '*') {
+                dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[n][m];
+};
