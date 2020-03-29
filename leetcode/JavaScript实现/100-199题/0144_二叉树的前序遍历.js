393948d32f9ef85381011ba59fe16c2c082b108a
zifans <903953451@qq.com>
2020-03-29 15:47:31 +0800

    前序遍历的递归和迭代方法

--- leetcode/JavaScript实现/100-199题/0144_二叉树的前序遍历.js
+++ leetcode/JavaScript实现/100-199题/0144_二叉树的前序遍历.js
@@ -1,4 +1,47 @@
-// 1、递归
+// 1、递归 最基本的
+var preorderTraversal = function(root) {
+    let ans = [];
 
+    let preOrder = (temp, arr) => {
+        if (temp == null)
+            return;
 
-// 2、迭代
+        arr.push(temp.val);
+        preOrder(temp.left, arr);
+        preOrder(temp.right, arr);
+
+        return arr;
+    };
+
+    preOrder(root, ans);
+
+    return ans;
+};
+
+// 2、迭代 考研也经常写非递归的
+var preorderTraversal = function(root) {
+    let ans = [];
+    let stack = [];
+
+    if (!root)
+        return [];
+    stack.push(root);
+
+    while (stack.length) {
+        let temp = stack.pop();
+        ans.push(temp.val);
+
+        if (temp.right)
+            stack.push(temp.right);
+        if (temp.left)
+            stack.push(temp.left);
+        /*
+        这两条语句是上面额if 语句意思一样
+        temp.right && stack.push(temp.right);
+        temp.left && stack.push(temp.left);
+        */
+    }
+
+    return ans;
+};
+
