b373dbd8ebf2e1819377b1317c94e54b7c6bdec8
zifans <903953451@qq.com>
2020-04-16 13:38:27 +0800

    链表的插入排序

--- leetcode/JavaScript实现/100-199题/0147_对链表进行插入排序.js
+++ leetcode/JavaScript实现/100-199题/0147_对链表进行插入排序.js
@@ -1,1 +1,24 @@
-// 1、
+var insertionSortList = function(head) {
+    let dummy = new ListNode(null);
+    dummy.next = head;
+    let p = dummy.next;
+
+    while (p && p.next) {
+        if (p.next.val >= p.val) {
+            p = p.next;
+            continue;
+        }
+
+        let pre = dummy;
+        while (pre.next.val < p.next.val) {
+            pre = pre.next;
+        }
+
+        let temp = p.next;
+        p.next = temp.next;
+        temp.next = pre.next;
+        pre.next = temp;
+    }
+
+    return dummy.next;
+};
