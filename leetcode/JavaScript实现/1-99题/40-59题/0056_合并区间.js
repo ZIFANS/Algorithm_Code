fdfbf6823efcc5f00f3ad54fe7dae483c74ff140
zifans <903953451@qq.com>
2020-04-16 13:13:55 +0800

    合并区间

--- leetcode/JavaScript实现/1-99题/40-59题/0056_合并区间.js
+++ leetcode/JavaScript实现/1-99题/40-59题/0056_合并区间.js
@@ -1,10 +1,11 @@
 var merge = function(intervals) {
-    if (!intervals || !intervals.length)
+    if (!intervals.length)
         return [];
     intervals.sort((a, b) => {
         return a[0] - b[0];
     });
     let ans = [intervals[0]];
+
     for (let i = 1; i < intervals.length; ++i) {
         if (ans[ans.length - 1][1] >= intervals[i][0]) {
             ans[ans.length - 1][1] = Math.max(ans[ans.length - 1][1], intervals[i][1]);
@@ -12,5 +13,6 @@ var merge = function(intervals) {
             ans.push(intervals[i]);
         }
     }
+
     return ans;
 };
