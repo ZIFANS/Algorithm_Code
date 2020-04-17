94bee151e9434bd0997945871bbaf893510739b7
zifans <903953451@qq.com>
2020-04-17 13:27:04 +0800

    贪心算法

--- leetcode/JavaScript实现/1-99题/40-59题/0055_跳跃游戏.js
+++ leetcode/JavaScript实现/1-99题/40-59题/0055_跳跃游戏.js
@@ -1,10 +1,12 @@
-// 贪心算法
+// 1?
 var canJump = function(nums) {
     let lastPos = nums.length - 1;
+
     for (let i = nums.length - 1; i >= 0; --i) {
         if (i + nums[i] >= lastPos) {
             lastPos = i;
         }
     }
+
     return lastPos === 0;
 };
