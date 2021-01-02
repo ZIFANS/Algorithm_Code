3266aa1308516c11bd6fd8fdb420d677f9edbb6d
zifans <903953451@qq.com>
2021-01-02 13:38:00 +0800

    三种语言刷题写到一个Markdown文件

--- leetcode/JavaScript实现/100-199题/0104.js
+++
@@ -1,8 +0,0 @@
-// 第一想法就是递归 一次AC
-var maxDepth = function(root) {
-    if(root === null)
-        return 0;
-    let left = maxDepth(root.left);
-    let right = maxDepth(root.right);
-    return left > right ? left + 1 : right + 1;
-};
